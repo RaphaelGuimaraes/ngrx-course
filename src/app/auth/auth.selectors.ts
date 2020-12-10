2bfea4867387f9b7475a5968112ca470b646ba8a
Raphael Guimarães <raphaguimaraes@gmail.com>
2020-12-10 19:28:35 -0300

    create actions, reducers & effects to auth module

---
+++ src/app/auth/auth.selectors.ts
@@ -0,0 +1,12 @@
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { AuthState } from "./reducers";
+
+// feature selectors
+export const selectAuthState = createFeatureSelector<AuthState>("auth");
+
+export const isLoggedIn = createSelector(
+  selectAuthState, // (state) => state["auth"]
+  (auth) => !!auth.user
+);
+
+export const isLoggedOut = createSelector(isLoggedIn, (loggedIn) => !loggedIn);
